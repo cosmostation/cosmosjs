4b212e169fd93337a3c288438c40d5074a71a7c5
okwme <billy.rennekamp@gmail.com>
2020-01-03 03:25:49 +0100

    fixes node-fetch for webpack

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@
 
 'use strict'
 
-const fetch = require("node-fetch");
+const fetch = require('node-fetch').default
 const bip39 = require('bip39');
 const bip32 = require('bip32');
 const bech32 = require('bech32');
