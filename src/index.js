6b53456792a5f21c4495045aae73f8e2ed176d55
Booyoun-Kim <booyoun@stamper.network>
2021-02-20 11:10:29 +0900

    added the account api for stargate-final chain-id

--- src/index.js
+++ src/index.js
@@ -82,6 +82,8 @@ Cosmos.prototype.getAccounts = function(address) {
 	let accountsApi = "";
 	if (this.chainId.indexOf("cosmos") != -1) {
 		accountsApi = "/cosmos/auth/v1beta1/accounts/";
+	} else if (this.chainId.indexOf("stargate-final") != -1) {
+		accountsApi = "/cosmos/auth/v1beta1/accounts/";
 	} else if (this.chainId.indexOf("irishub") != -1) {
 		accountsApi = "/bank/accounts/";
 	} else {
